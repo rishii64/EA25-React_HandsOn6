ee441dca350b707f6bfc1bbb645961802e02799e
Saptarsi <saptarsismmitra@gmail.com>
2023-10-19 17:30:32 +0530

    updated...

--- src/EditStudent.js
+++ src/EditStudent.js
@@ -6,9 +6,6 @@ function EditStudent(){
     const contextData = useContext(Store)
     const index = useParams().id
     const nav = useNavigate()
-    // console.log(contextData);
-    // console.log(index);
-    // console.log(contextData.data[index]);
 
     const updateObj = {
         name : contextData.data[index].name,
@@ -18,29 +15,41 @@ function EditStudent(){
     }
 
     const handleChange = (e)=>{
-        // console.log(e.target.name);
         updateObj[e.target.name] = e.target.value
     }
 
     const handleSubmit = ()=>{
-        // console.log(updateObj);
         contextData.data[index] = updateObj
         nav('/student')
     }
     return(
         <>
-            <h1>This is Edit Form</h1>
-            <label>Name</label>
-            <input type="text" placeholder={contextData.data[index].name} name='name' onChange={handleChange}/> <br/>
-            <label>Age</label>
-            <input type="number" placeholder={contextData.data[index].age} name='age' onChange={handleChange}/> <br/>
-            <label>Course</label>
-            <input type="text" placeholder={contextData.data[index].course} name='course' onChange={handleChange}/> <br/>
-            <label>Batch</label>
-            <input type="text" placeholder={contextData.data[index].batch} name='batch' onChange={handleChange}/> <br/>
-
-            <button onClick={()=> nav('/student')}>Back</button>
-            <button onClick={handleSubmit}>Update</button>
+            <h1>Edit Form</h1>
+            <div id='inputDetails'>
+                <span>
+                    <label>Name: </label>
+                    <input type="text" placeholder={contextData.data[index].name} name='name' onChange={handleChange}/> <br/>
+                </span>
+                <span>
+                    <label>Age: </label>
+                    <input type="number" placeholder={contextData.data[index].age} name='age' onChange={handleChange}/> <br/>
+                </span>
+                
+            </div>
+            <div id='inputDetails'>
+                <span>
+                    <label>Course: </label>
+                    <input type="text" placeholder={contextData.data[index].course} name='course' onChange={handleChange}/> <br/>
+                </span>
+                <span>
+                    <label>Batch</label>
+                    <input type="text" placeholder={contextData.data[index].batch} name='batch' onChange={handleChange}/> <br/>
+                </span>
+            </div>
+            <div id='inputDetails'>
+                <button id='back' onClick={()=> nav('/student')}>Back</button>
+                <button id='update' onClick={handleSubmit}>Update</button>
+            </div>
         </>
     )
 }
--- src/StuContact.js
+++ src/StuContact.js
@@ -21,22 +21,35 @@ export default function StuContact() {
       contextData.data.push(newObj);
       navigate('/student');
     }
-    // console.log(contextData);
 
   return (
     <div id='data'>
+      <h1>New Student Form :</h1>
       <div id='inputDetails'>
-        <input type="text" name='name' placeholder='Enter Student Name' className='stuDescInput' onChange={handleChange} />
-        <input type="number" name='age' placeholder='Enter Student Age' className='stuDescInput' onChange={handleChange} />
+        <span>
+          <label>Name: </label>
+          <input type="text" name='name' placeholder='Enter Student Name' className='stuDescInput' onChange={handleChange} />
+        </span>
+        <span>
+          <label>Age: </label>
+          <input type="number" name='age' placeholder='Enter Student Age' className='stuDescInput' onChange={handleChange} />
+        </span>
+      </div>
+      
+      <div id='inputDetails'>
+        <span>
+          <label>Course: </label>
+          <input type="text" name='course' placeholder="Enter Student's Course" className='stuDescInput' onChange={handleChange} />
+        </span>
+        <span>
+          <label>Batch: </label>
+          <input type="text" name='batch' placeholder="Enter Student's Batch" className='stuDescInput' onChange={handleChange} />
+        </span>
       </div>
       <div id='inputDetails'>
-        <input type="text" name='course' placeholder="Enter Student's Course" className='stuDescInput' onChange={handleChange} />
-        <input type="text" name='batch' placeholder="Enter Student's Batch" className='stuDescInput' onChange={handleChange} />
+        <button id='back' onClick={()=> navigate(-1)}>Back</button>
+        <button id='update' onClick={handleSubmit}>Submit</button>
       </div>
-        <div id='inputDetails'>
-          <button id='back' onClick={()=> navigate(-1)}>Back</button>
-          <button id='update' onClick={handleSubmit}>Submit</button>
-        </div>
     </div>
   )
 }
